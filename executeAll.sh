405d596beca1dfd5e9591b61957041bd482e2975
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-04 16:26:55 +0200

    Only select last row

--- executeAll.sh
+++ executeAll.sh
@@ -48,7 +48,7 @@ echo "::::::::::::::::::::GETCHANGES::::::::::::::::::::::::::::::::::::::::"
 ) && true
 
 # If minor updates to the project occur, the version name may change
-version=$(cat results/execute_demo-project.json | grep "versions" -A 4 | tail -n 2 | tr -d "\": {")
+version=$(cat results/execute_demo-project.json | grep "versions" -A 4 | tail -n 1 | tr -d "\": {")
 echo "Version: $version"
 
 echo "::::::::::::::::::::SEARCHCAUSE:::::::::::::::::::::::::::::::::::::::"
