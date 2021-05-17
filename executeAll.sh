98902310572d074512b24bce7f79ec5a526b851d
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-05-17 13:01:58 +0200

    Using upper case for all variables

--- executeAll.sh
+++ executeAll.sh
@@ -7,7 +7,7 @@ git clone https://github.com/DaGeRe/peass.git && \
 DEMO_HOME=$(pwd)/../demo-project
 DEMO_PROJECT_PEASS=../demo-project_peass
 EXECUTION_FILE=results/execute_demo-project.json
-right_sha="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
+RIGHT_SHA="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
 
 # It is assumed that $DEMO_HOME is set correctly and PeASS has been built!
 echo ":::::::::::::::::::::SELECT:::::::::::::::::::::::::::::::::::::::::::"
@@ -23,7 +23,7 @@ then
 	ls ../demo-project_peass/projectTemp/1_peass/
 	ls ../demo-project_peass/projectTemp/1_peass/logs/
 	cat ../demo-project_peass/projectTemp/1_peass/logs/bf6d4897d8b13dcdc23d0e29d9b3b1791dec9d34/*/* 
-	cat ../demo-project_peass/projectTemp/1_peass/logs/$right_sha/*/*
+	cat ../demo-project_peass/projectTemp/1_peass/logs/$RIGHT_SHA/*/*
 	exit 1
 fi
 
@@ -34,8 +34,8 @@ echo "::::::::::::::::::::GETCHANGES::::::::::::::::::::::::::::::::::::::::"
 ./peass getchanges -data $DEMO_PROJECT_PEASS -dependencyfile results/deps_demo-project.json
 
 #Check, if changes_demo-project.json contains the correct commit-SHA
-test_sha=$(grep -A1 'versionChanges" : {' results/changes_demo-project.json | grep -v '"versionChanges' | grep -Po '"\K.*(?=")')
-if [ "$right_sha" != "$test_sha" ]
+TEST_SHA=$(grep -A1 'versionChanges" : {' results/changes_demo-project.json | grep -v '"versionChanges' | grep -Po '"\K.*(?=")')
+if [ "$RIGHT_SHA" != "$TEST_SHA" ]
 then
     echo "commit-SHA is not equal to the SHA in changes_demo-project.json!"
     cat results/statistics/demo-project.json
@@ -45,31 +45,31 @@ else
 fi
 
 # If minor updates to the project occur, the version name may change
-version=$(grep '"testcases" :' -B 1 $EXECUTION_FILE | head -n 1 | tr -d "\": {")
-echo "Version: $version"
+VERSION=$(grep '"testcases" :' -B 1 $EXECUTION_FILE | head -n 1 | tr -d "\": {")
+echo "VERSION: $VERSION"
 
 echo "::::::::::::::::::::SEARCHCAUSE:::::::::::::::::::::::::::::::::::::::"
-./peass searchcause -vms 5 -iterations 1 -warmup 0 -version $version -test de.test.CalleeTest\#onlyCallMethod1 -folder $DEMO_HOME -executionfile $EXECUTION_FILE
+./peass searchcause -vms 5 -iterations 1 -warmup 0 -version $VERSION -test de.test.CalleeTest\#onlyCallMethod1 -folder $DEMO_HOME -executionfile $EXECUTION_FILE
 
 echo "::::::::::::::::::::VISUALIZERCA::::::::::::::::::::::::::::::::::::::"
 ./peass visualizerca -data ../demo-project_peass -propertyFolder results/properties_demo-project/
 
 #Check, if a slowdown is detected for innerMethod
-state=$(grep '"call" : "de.test.Callee#innerMethod",\|state' results/$version/de.test.CalleeTest_onlyCallMethod1.js | grep "innerMethod" -A 1 | grep '"state" : "SLOWER",' | grep -o 'SLOWER')
-if [ "$state" != "SLOWER" ]
+STATE=$(grep '"call" : "de.test.Callee#innerMethod",\|state' results/$VERSION/de.test.CalleeTest_onlyCallMethod1.js | grep "innerMethod" -A 1 | grep '"state" : "SLOWER",' | grep -o 'SLOWER')
+if [ "$STATE" != "SLOWER" ]
 then
-    echo "State for de.test.Callee#innerMethod in de.test.CalleeTest#onlyCallMethod1.html has not the expected value SLOWER, but was $state!"
-    cat results/$version/de.test.CalleeTest_onlyCallMethod1.js
+    echo "State for de.test.Callee#innerMethod in de.test.CalleeTest#onlyCallMethod1.html has not the expected value SLOWER, but was $STATE!"
+    cat results/$VERSION/de.test.CalleeTest_onlyCallMethod1.js
     exit 1
 else
     echo "Slowdown is detected for innerMethod."
 fi
 
-sourceMethodLine=$(grep "de.test.Callee.method1_" results/$version/de.test.CalleeTest_onlyCallMethod1.js -A 3 | head -n 3 | grep innerMethod)
-if [[ "$sourceMethodLine" != *"innerMethod();" ]]
+SOURCE_METHOD_LINE=$(grep "de.test.Callee.method1_" results/$VERSION/de.test.CalleeTest_onlyCallMethod1.js -A 3 | head -n 3 | grep innerMethod)
+if [[ "$SOURCE_METHOD_LINE" != *"innerMethod();" ]]
 then
     echo "Line could not be detected - source reading probably failed."
     echo "Line: "
-    echo $sourceMethodLine
+    echo "SOURCE_METHOD_LINE: $SOURCE_METHOD_LINE"
     exit 1
 fi
