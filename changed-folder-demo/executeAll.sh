8e28ae47fafaf85df52cc2f50b3f3ff6f687593b
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-01-13 22:32:56 +0100

    Add changed folder demo

---
+++ changed-folder-demo/executeAll.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+DEMO_PROJECT_NAME=demo-project
+
+if [ "$#" -lt 1 ]; then
+	branch="main"
+else
+	branch=$1
+fi
+
+if [ ! -z $2 ]; then
+    rcaStrategy=$2
+else
+    rcaStrategy="COMPLETE"
+fi
+
+tar -xf "$DEMO_PROJECT_NAME".tar.xz
+
+source ../common-functions.sh
+
+DEMO_HOME=$(pwd)/$DEMO_PROJECT_NAME
+DEMO_PROJECT_PEASS=$(pwd)/"$DEMO_PROJECT_NAME"_peass
+EXECUTION_FILE=results/execute_"$DEMO_PROJECT_NAME".json
+DEPENDENCY_FILE=results/deps_"$DEMO_PROJECT_NAME".json
+CHANGES_DEMO_PROJECT=results/changes_"$DEMO_PROJECT_NAME".json
+PROPERTY_FOLDER=results/properties_"$DEMO_PROJECT_NAME"/
+
+
+VERSION="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
+
+# It is assumed that $DEMO_HOME is set correctly and PeASS has been built!
+echo ":::::::::::::::::::::SELECT:::::::::::::::::::::::::::::::::::::::::::"
+java -jar $PEASS_FILE select -folder $DEMO_HOME -notUseSourceInstrumentation -testClassFolder src/myTestName/java
+
+INITIALVERSION="a4e04a268991230f0d29ba801e6703ab5c6ce5f6"
+INITIAL_SELECTED=$(grep "initialversion" -A 1 $DEPENDENCY_FILE | grep "\"version\"" | tr -d " \"," | awk -F':' '{print $2}')
+if [ "$INITIAL_SELECTED" != "$INITIALVERSION" ]
+then
+	echo "Initialversion should be $INITIALVERSION, but was $INITIAL_SELECTED"
+	exit 1
+fi
+
+if [ ! -f "$EXECUTION_FILE" ]
+then
+    echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    echo "$EXECUTION_FILE could not be found!"
+    echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    exit 1
+fi
+
+echo ":::::::::::::::::::::MEASURE::::::::::::::::::::::::::::::::::::::::::"
+java -jar $PEASS_FILE measure -executionfile $EXECUTION_FILE -folder $DEMO_HOME -vms 5 -iterations 5 -warmup 5 -repetitions 5 -testClassFolder src/myTestName/java
+
+echo "::::::::::::::::::::GETCHANGES::::::::::::::::::::::::::::::::::::::::"
+java -jar $PEASS_FILE getchanges -data $DEMO_PROJECT_PEASS -dependencyfile $DEPENDENCY_FILE
+
+#Check, if $CHANGES_DEMO_PROJECT contains the correct commit-SHA
+TEST_SHA=$(grep -A1 'versionChanges" : {' $CHANGES_DEMO_PROJECT | grep -v '"versionChanges' | grep -Po '"\K.*(?=")')
+if [ "$VERSION" != "$TEST_SHA" ]
+then
+    echo "commit-SHA ("$VERSION") is not equal to the SHA in $CHANGES_DEMO_PROJECT ("$TEST_SHA")!"
+    cat results/statistics/"$DEMO_PROJECT_NAME".json
+    exit 1
+else
+    echo "$CHANGES_DEMO_PROJECT contains the correct commit-SHA."
+fi
+
+echo "::::::::::::::::::::SEARCHCAUSE:::::::::::::::::::::::::::::::::::::::"
+echo "rcaStrategy is: $rcaStrategy"
+java -jar $PEASS_FILE searchcause -vms 3 -iterations 5 -warmup 1 -repetitions 5 -version $VERSION \
+    -test de.dagere.peass.ExampleTest\#test \
+    -folder $DEMO_HOME \
+    -executionfile $EXECUTION_FILE \
+    -rcaStrategy $rcaStrategy \
+    -propertyFolder $PROPERTY_FOLDER \
+    -testClassFolder src/myTestName/java
+
+echo "::::::::::::::::::::VISUALIZERCA::::::::::::::::::::::::::::::::::::::"
+java -jar $PEASS_FILE visualizerca -data $DEMO_PROJECT_PEASS -propertyFolder $PROPERTY_FOLDER
+
+#Check, if a slowdown is detected for Callee#innerMethod
+STATE=$(grep -A21 '"call" : "de.dagere.peass.Callee#innerMethod",' results/$VERSION/de.dagere.peass.ExampleTest_test.js \
+    | grep '"state" : "SLOWER",' \
+    | grep -o 'SLOWER')
+if [ "$STATE" != "SLOWER" ]
+then
+    echo "State for Callee#innerMethod in de.dagere.peass.ExampleTest_test.js has not the expected value SLOWER, but was $STATE!"
+    cat results/$VERSION/de.dagere.peass.ExampleTest_test.js
+    exit 1
+else
+    echo "Slowdown is detected for Callee#innerMethod."
+fi
+
+SOURCE_METHOD_LINE=$(grep "Callee.method1_" results/$VERSION/de.dagere.peass.ExampleTest_test.js -A 3 \
+    | head -n 3 \
+    | grep innerMethod)
+if [[ "$SOURCE_METHOD_LINE" != *"innerMethod();" ]]
+then
+    echo "Line could not be detected - source reading probably failed."
+    echo "SOURCE_METHOD_LINE: $SOURCE_METHOD_LINE"
+    exit 1
+else
+    echo "SOURCE_METHOD_LINE is correct."
+fi
