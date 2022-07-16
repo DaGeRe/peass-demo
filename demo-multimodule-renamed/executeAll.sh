6149ad654935958647a24ad811eaf4e35ded410c
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-07-16 11:35:04 +0200

    Adapt to changed folder structure

--- demo-multimodule-renamed/executeAll.sh
+++ demo-multimodule-renamed/executeAll.sh
@@ -64,19 +64,19 @@ echo "::::::::::::::::::::VISUALIZERCA::::::::::::::::::::::::::::::::::::::"
 java -jar $PEASS_FILE visualizerca -data $DEMO_PROJECT_PEASS -propertyFolder $PROPERTY_FOLDER
 
 #Check, if a slowdown is detected for Callee#innerMethod
-STATE=$(grep -A21 '"call" : "de.AnotherTest#testMe",' results/$VERSION/module-using§de.AnotherTest_testMe.js \
+STATE=$(grep -A21 '"call" : "de.AnotherTest#testMe",' results/$VERSION/module-using§de.AnotherTest/testMe.js \
     | grep '"state" : "SLOWER",' \
     | grep -o 'SLOWER')
 if [ "$STATE" != "SLOWER" ]
 then
     echo "State for de.AnotherTest#testMe in module-using§de.AnotherTest_testMe.js has not the expected value SLOWER, but was $STATE!"
-    cat results/$COMMIT/de.dagere.peass.ExampleTest_test.js
+    cat results/$COMMIT/de.dagere.peass.ExampleTest/test.js
     exit 1
 else
     echo "Slowdown is detected for Callee#innerMethod."
 fi
 
-SOURCE_METHOD_LINE=$(grep "AnotherChangeable.callMe_" results/$COMMIT/module-using§de.AnotherTest_testMe.js -A 3 \
+SOURCE_METHOD_LINE=$(grep "AnotherChangeable.callMe_" results/$COMMIT/module-using§de.AnotherTest/testMe.js -A 3 \
     | head -n 3 \
     | grep callMe)
 if [[ "$SOURCE_METHOD_LINE" != *"callMe() {" ]]
