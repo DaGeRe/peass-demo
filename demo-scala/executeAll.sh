0b57bb8ab0b9151751257f65cbd94e87f1b7a0e5
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-03-30 11:03:22 +0200

    Add scala demo project - preparation for Peass scala support

---
+++ demo-scala/executeAll.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+DEMO_PROJECT_NAME=project-scala
+
+if [ "$#" -lt 1 ]; then
+	branch="main"
+else
+	branch=$1
+fi
+
+if [ ! -z $2 ]; then
+    rcaStrategy=$2
+else
+    rcaStrategy="COMPLETE"
+fi
+
+tar -xf "$DEMO_PROJECT_NAME".tar.xz
+
+source ../common-functions.sh
+
+DEMO_HOME=$(pwd)/$DEMO_PROJECT_NAME
+DEMO_PROJECT_PEASS=$(pwd)/"$DEMO_PROJECT_NAME"_peass
+EXECUTION_FILE=results/traceTestSelection_"$DEMO_PROJECT_NAME".json
+DEPENDENCY_FILE=results/staticTestSelection_"$DEMO_PROJECT_NAME".json
+CHANGES_DEMO_PROJECT=results/changes.json
+PROPERTY_FOLDER=results/properties_"$DEMO_PROJECT_NAME"/
+
+COMMIT="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
+
+# It is assumed that $DEMO_HOME is set correctly and Peass has been built!
+echo ":::::::::::::::::::::SELECT:::::::::::::::::::::::::::::::::::::::::::"
+java -jar $PEASS_FILE select -folder $DEMO_HOME
+
+INITIALCOMMIT="0133f8a6ff22483d59415e3193a463eb5c076716"
+checkInitialCommit $INITIALCOMMIT $DEPENDENCY_FILE
+
+if [ ! -f "$EXECUTION_FILE" ]
+then
+    echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    echo "$EXECUTION_FILE could not be found!"
+    echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    exit 1
+fi
+
+echo ":::::::::::::::::::::MEASURE::::::::::::::::::::::::::::::::::::::::::"
+java -jar $PEASS_FILE measure -executionFile $EXECUTION_FILE -folder $DEMO_HOME -vms 5 -iterations 5 -warmup 5 -repetitions 5
+
+echo "::::::::::::::::::::GETCHANGES::::::::::::::::::::::::::::::::::::::::"
+java -jar $PEASS_FILE getchanges -data $DEMO_PROJECT_PEASS -staticSelectionFile $DEPENDENCY_FILE
+
+checkChanges $COMMIT $CHANGES_DEMO_PROJECT $DEMO_PROJECT_NAME
+
+echo "::::::::::::::::::::SEARCHCAUSE:::::::::::::::::::::::::::::::::::::::"
+echo "rcaStrategy is: $rcaStrategy"
+java -jar $PEASS_FILE searchcause -vms 5 -iterations 5 -warmup 5 -repetitions 5 -commit $COMMIT \
+    -test de.dagere.peass.ExampleTest\#test \
+    -folder $DEMO_HOME \
+    -executionFile $EXECUTION_FILE \
+    -rcaStrategy $rcaStrategy \
+    -propertyFolder $PROPERTY_FOLDER \
+    -writingType BinarySimple
+
+echo "::::::::::::::::::::VISUALIZERCA::::::::::::::::::::::::::::::::::::::"
+java -jar $PEASS_FILE visualizerca -data $DEMO_PROJECT_PEASS -propertyFolder $PROPERTY_FOLDER
+
+checkResultJS $COMMIT
